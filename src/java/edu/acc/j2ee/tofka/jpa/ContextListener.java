a06701d9f31d1875e6e9300457f130eb63758ba1
bc56075 <bc56075@Lenovo-PC.attlocal.net>
2015-05-25 18:57:35 -0500

    ContextListener works.

--- src/java/edu/acc/j2ee/tofka/jpa/ContextListener.java
+++ src/java/edu/acc/j2ee/tofka/jpa/ContextListener.java
@@ -4,11 +4,14 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        System.out.print("----------------------------------- CONTEXT INITIALIZED");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("TOFKAPU");
         sce.getServletContext().setAttribute("emf", emf);
     } 
